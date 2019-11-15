5c0be4610687bf53940e70604abab54dbbc57bf3
Artem Dorokhov <artdor1978@gmail.com>
2019-11-15 11:01:24 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { connect } from "react-redux";
+import Counter from "./Counter";
+function mapStateToProps(state) {
+	return {
+		countValue: state.count
+	};
 }
-
-export default App;
+var increaseAction = { type: "increase" };
+var decreaseAction = { type: "decrease" };
+function mapDispatchToProps(dispatch) {
+	return {
+		increaseCount: function() {
+			return dispatch(increaseAction);
+		},
+		decreaseCount: function() {
+			return dispatch(decreaseAction);
+		}
+	};
+}
+var connectedComponent = connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default connectedComponent;
